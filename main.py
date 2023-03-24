c595353771e17ccd621f579d4ac99cc1554cad05
johnjoshuakjc <99176367+johnjoshuakjc@users.noreply.github.com>
2023-03-24 11:38:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
+import sklearn
+#from sklearn.linear_model import LinearRegression
 import datetime as dt
 
 # Set the page title
